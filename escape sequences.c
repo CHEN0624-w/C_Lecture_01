fc1fa148e3251d3585dc691cdc9b141a940196cb
CHEN0624-w <cxq20071210@outlook.com>
2025-10-13 13:43:23 +0800

    escape sequences

---
+++ escape sequences.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello\nworld\n");
+    printf("name:\txiaoming\nage:\t20\n");
+    printf("\\\n");
+    printf("Hello\n\0world\n ");
+    printf("\aringring\n");
+    printf("123456\n");
+    printf("1234\b\b56\n");
+    printf("123\r456\n");
+    printf("vertical\vtab\n");
+}
