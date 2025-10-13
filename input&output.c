642043506c9ad29ec8cadf10825e5604575c40ab
CHEN0624-w <cxq20071210@outlook.com>
2025-10-13 15:09:18 +0800

    third save

---
+++ input&output.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int integer1;
+    int integer2;
+        
+    printf("Enter first integer\n");
+    scanf("%d",&integer1);
+   
+    printf("enter second integer\n");
+    scanf("%d",&integer2);
+   
+    int sum;
+    sum=integer1+integer2;
+    printf("sum is %d\n",sum);
+
+}
