c80ea105651d34f82eeb453fe58b5da0e2b23552
CHEN0624-w <cxq20071210@outlook.com>
2025-10-14 13:03:18 +0800

    ex3

---
+++ ex3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Input and Output\n~~~~~~~~~~~~\n");
+
+    int x,y,z;
+
+    printf("Insert No 1:");
+    scanf("%d",&x);
+
+    printf("\nInsert No 2:");
+    scanf("%d",&y);
+
+    printf("\nInsert No 3:");
+    scanf("%d",&z);
+
+    printf("Result:\n");
+    printf("%d\n\t%d\n\t\t%d",x,y,z);
+
+    return 0;
+} 
